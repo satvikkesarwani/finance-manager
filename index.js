9c1fe84afe4fbb3d7fa9ad184e98fa676256a764
satvikkesarwani <217068858+satvikkesarwani@users.noreply.github.com>
2026-03-19 13:03:30 +0530

    fix: use compatible wildcard route for express

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use("/api/bill", billRoute)
 app.use(express.static(path.join(__dirname, "client/dist")))
 
 // Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
+app.get("(.*)", (req, res) => {
     res.sendFile(path.join(__dirname, "client/dist", "index.html"))
 })
 
